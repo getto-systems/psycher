83409f0db7d769a9397e507b6d22da6f030afdd3
shun <shun.fix9@gmail.com>
2019-08-04 20:33:49 +0000

    fix: test case

--- test/secrets/slack.test.js
+++ test/secrets/slack.test.js
@@ -1,20 +1,5 @@
 const slack_secret = require("../../lib/secrets/slack");
 
-/**
- * secret : {
- *   bot_token: slack bot token
- * }
- * struct : {
- *   channel: slack channel
- *   timestamp: event timestamp
- * }
- *
- * returns {
- *   channel,
- *   timestamp,
- *   bot_token,
- * }
- */
 test("properties", () => {
   const raw_secret = {
     bot_token: "SLACK_BOT_TOKEN",
