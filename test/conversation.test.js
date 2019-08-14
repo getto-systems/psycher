791d8ee0d6ee2ef75bde250be6f9a9d6e4eed78e
shun <shun.fix9@gmail.com>
2019-08-14 07:16:36 +0000

    fix: has_no_deploy_target

--- test/conversation.test.js
+++ test/conversation.test.js
@@ -32,7 +32,7 @@ test("init conversation", async () => {
   expect(conversation.includes_some(["deploy"])).toBe(true);
 
   expect(await conversation.is_already_started()).toBe(false);
-  expect(await conversation.has_deploy_target()).toBe(true);
+  expect(await conversation.has_no_deploy_target()).toBe(false);
 });
 
 test("nothing effect on double is_already_started check", async () => {
